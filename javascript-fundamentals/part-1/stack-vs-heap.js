93bb05229735beaf8c2f2d540955fae0ea710fb4
Jardel Matias <jardelmatias@live.com>
2021-09-13 23:35:33 -0300

    add memory vs heap

---
+++ javascript-fundamentals/part-1/stack-vs-heap.js
@@ -0,0 +1,10 @@
+// Primitives types are stored in stack
+// https://developer.mozilla.org/en-US/docs/Glossary/Primitive
+
+// const myVar = 'This will exists only in stack'
+
+// Objects, functions, arrays and all non primitives values are stored in head, indeed all objects that can increase in side are store in head.
+
+// const newObject = {}
+
+// newObject will be created in stack and guard a reference to heap, in heap it can grow dinamicly
