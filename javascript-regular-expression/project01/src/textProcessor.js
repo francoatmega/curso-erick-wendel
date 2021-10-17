b5656c4b87bf0c0259f7539a88ac18e8648e208a
Jardel Matias <jardelmatias@live.com>
2021-10-17 12:58:51 -0300

    feat: add splitIntoColumns and strigString methods

--- javascript-regular-expression/project01/src/textProcessor.js
+++ javascript-regular-expression/project01/src/textProcessor.js
@@ -1,3 +1,4 @@
+const { evaluateRegex } = require('../src/helpers')
 class TextProcessor {
 
     #content
@@ -7,8 +8,20 @@ class TextProcessor {
     }
 
     extractHirerAndHireData() {
-        const matchRegex = /(?<=[CONTRATANTE|CONTRATADA]:\s)[^\s](.*\n.*)$/gmi
+        const matchRegex = evaluateRegex(/(?<=[CONTRATANTE|CONTRATADA]:\s)[^\s](.*\n.*)$/gmi)
         this.#content= this.#content.match(matchRegex)
+        return this
+    }
+
+    splitIntoColumns() {
+        const splitRegex = evaluateRegex(/,/)
+        this.#content = this.#content.map(item => item.split(splitRegex))
+        return this
+    }
+
+    stripString() {
+        const trimSpacesAndBreakLines = evaluateRegex(/^\s+|\s+$|\n/g)
+        this.#content = this.#content.map(person => person.map(line => line.replace(trimSpacesAndBreakLines, '')))
         return this
     }
 
--- javascript-regular-expression/project01/tests/textProcessor.spec.js
+++ javascript-regular-expression/project01/tests/textProcessor.spec.js
@@ -17,4 +17,64 @@ describe('Test of TextProcessor class', () => {
         ]
         expect(resultExtracted).to.have.members(expected)
     })
+    it('should divide extracted data in columns', () => {
+        const expected = [
+            'Xuxa da Silva',
+            ' brasileira',
+            ' casada', 
+            ' CPF 235.743.420-12',
+            ' residente e \ndomiciliada a Rua dos bobos',
+            ' zero',
+            ' bairro Alphaville',
+            ' São Paulo. '
+        ]
+        const resultColumns = new TextProcessor(validHirerAndHire).extractHirerAndHireData().splitIntoColumns().build()
+        expect(resultColumns).to.be.an('array').deep.include(expected)
+    })
+    it('should trim spaces and remove break lines', () => {
+        const content = [
+            [
+              'Xuxa da Silva',
+              ' brasileira',
+              ' casada',
+              ' CPF 235.743.420-12',
+              ' residente e \ndomiciliada a Rua dos bobos',
+              ' zero',
+              ' bairro Alphaville',
+              ' São Paulo. '
+            ],
+            [
+              'Arya Robbin',
+              ' belga',
+              ' casado',
+              ' CPF 884.112.200-52',
+              ' residente e \ndomiciliada a Av. paulista',
+              ' 1400',
+              ' bairro Consolação',
+              ' São Paulo. '
+            ],
+            [
+              'Júlia Menezes',
+              ' brasileira',
+              ' solteira',
+              ' CPF 297.947.800-81',
+              ' residente e \ndomiciliada a Av. dos Estados',
+              ' 99',
+              ' bairro Jardins',
+              ' São Paulo.'
+            ]
+        ]
+        const expected = [
+            'Xuxa da Silva',
+            'brasileira',
+            'casada',
+            'CPF 235.743.420-12',
+            'residente e domiciliada a Rua dos bobos',
+            'zero',
+            'bairro Alphaville',
+            'São Paulo.'
+        ]
+        const result = new TextProcessor(content).stripString().build()
+        expect(result).to.be.an('array').deep.include(expected)
+    })
 }) 
